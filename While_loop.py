e5e60d3d15a8c21be9eb0bd66d28ac280fa008e0
vikash kunj <vikunj@deloitte.com>
2023-02-10 13:06:52 +0530

    while loop advance and function basic

--- While_loop.py
+++ While_loop.py
@@ -1,9 +1,18 @@
-a = 0;
+# a = 0;
+#
+# while a < 5:
+#     print(a)
+#     a = a + 1
+#
+# for a in range(10):
+#     print(f"{'inside for loop range value : '} {a}")
 
-while a<5:
-    print(a)
-    a =a+1
+user_input = ''
 
-
-for a in range(10):
-    print(f"{'inside for loop range value : '} {a}")
+while user_input != '-1':
+    user_input = input("Guess the number between 1 to 100: ")
+    if 1 <= int(user_input) <= 100:
+        print(f"{'number is between 1 to 100: '} {user_input}")
+    else:
+        print(f"{'number is: '}{user_input}")
+    print("let play again!")
---
+++ function_basic.py
@@ -0,0 +1,5 @@
+def first_function():
+    print("first function")
+
+
+first_function()
