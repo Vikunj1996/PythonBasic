6c466d77895e10618e46f4bcf60323e6c94755f7
vikash kunj <vikunj@deloitte.com>
2023-02-10 14:15:50 +0530

    function with argument

--- function_basic.py
+++ function_basic.py
@@ -1,5 +1,18 @@
-def first_function():
-    print("first function")
+# def first_function(name):
+#     print(f"{'your name: '}{name}")
+#
+#
+# first_function('vikash')
+# \n --> will add empty line
+
+def user_guessing_game(secret_number, stop):
+    user_input = ''
+    while user_input != stop:
+        user_input = input("\nenter the number: ")
+        if secret_number == user_input:
+            print(f"{'Bingo correct number: '} {secret_number}")
+        else:
+            print('number not matching! try again!! you number is ' + user_input)
 
 
-first_function()
+user_guessing_game("99", 'stop')
